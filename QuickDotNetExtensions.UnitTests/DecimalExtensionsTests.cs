d61ae4e41515e04a9fc13c5f348a142ef06d924d
Hatem El Moncer <hatemelmoncer@gmail.com>
2024-07-15 22:25:10 +0200

    Decimal extensions.

---
+++ QuickDotNetExtensions.UnitTests/DecimalExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿namespace QuickDotNetExtensions.UnitTests
+{
+    public class DecimalExtensionsTests
+    {
+        [Fact]
+        public void DecimalExtensions_RoundKeepOneDigit_Should_ReturnDecimalAndKeepOneDigit_RoundingBig()
+        {
+            decimal input = (decimal)10.788;
+            Assert.Equal((decimal)10.8, input.RoundKeepOneDigit());
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepOneDigit_Should_ReturnDecimalAndKeepOneDigit_RoundingSmall()
+        {
+            decimal input = (decimal)10.722;
+            Assert.Equal((decimal)10.7, input.RoundKeepOneDigit());
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepTwoDigits_Should_ReturnDecimalAndKeepTwoDigits_RoundingBig()
+        {
+            decimal input = (decimal)10.7788;
+            Assert.Equal((decimal)10.78, input.RoundKeepTwoDigits());
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepTwoDigits_Should_ReturnDecimalAndKeepTwoDigits_RoundingSmall()
+        {
+            decimal input = (decimal)10.7722;
+            Assert.Equal((decimal)10.77, input.RoundKeepTwoDigits());
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepThreeDigits_Should_ReturnDecimalAndKeepThreeDigits_RoundingBig()
+        {
+            decimal input = (decimal)10.7788;
+            Assert.Equal((decimal)10.779, input.RoundKeepThreeDigits());
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepThreeDigits_Should_ReturnDecimalAndKeepThreeDigits_RoundingSmall()
+        {
+            decimal input = (decimal)10.7722;
+            Assert.Equal((decimal)10.772, input.RoundKeepThreeDigits());
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepDigits_Should_ReturnDecimalAndKeepDigits_RoundingBig()
+        {
+            decimal input = (decimal)10.7788;
+            Assert.Equal((decimal)10.779, input.RoundKeepDigits(3));
+        }
+
+        [Fact]
+        public void DecimalExtensions_RoundKeepDigits_Should_ReturnDecimalAndKeepDigits_RoundingSmall()
+        {
+            decimal input = (decimal)10.7722;
+            Assert.Equal((decimal)10.772, input.RoundKeepDigits(3));
+        }
+    }
+}
---
+++ QuickDotNetExtensions/DecimalExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace QuickDotNetExtensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal RoundKeepOneDigit(this decimal value)
+        {
+            return decimal.Round(value, 1, MidpointRounding.AwayFromZero);
+        }
+
+        public static decimal RoundKeepTwoDigits(this decimal value)
+        {
+            return decimal.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+
+        public static decimal RoundKeepThreeDigits(this decimal value)
+        {
+            return decimal.Round(value, 3, MidpointRounding.AwayFromZero);
+        }
+
+        public static decimal RoundKeepDigits(this decimal value, int digitsToKeep)
+        {
+            return decimal.Round(value, digitsToKeep, MidpointRounding.AwayFromZero);
+        }
+    }
+}
